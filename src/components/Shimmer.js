9a8498bc4169dfbf53d46089939b0d02041f57d3
Anuj Kumar Mishra <87666135+Anuj236@users.noreply.github.com>
2023-08-07 17:40:03 +0530

    Add files via upload

---
+++ src/components/Shimmer.js
@@ -0,0 +1,18 @@
+const Shimmer=()=>{
+  <div className="w-64 h-64 bg-gray-100">
+    <div className="w-64 h-64 bg-gray-100"></div>
+    <div className="w-64 h-64 bg-gray-100"></div>
+    <div className="w-64 h-64 bg-gray-100"></div>
+    <div className="w-64 h-64 bg-gray-100"></div>
+    <div className="w-64 h-64 bg-gray-100"></div>
+    <div className="w-64 h-64 bg-gray-100"></div> 
+    <div className="w-64 h-64 bg-gray-100"></div>
+    <div className="w-64 h-64 bg-gray-100"></div>
+    <div className="w-64 h-64 bg-gray-100"></div>
+    <div className="w-64 h-64 bg-gray-100"></div>
+    <div className="w-64 h-64 bg-gray-100"></div>
+  </div>
+}
+
+export default Shimmer;
+
